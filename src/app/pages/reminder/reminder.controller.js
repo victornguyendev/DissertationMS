2f888bd1b6284a54b2078993e6895aa41187e5a5
ga <trandinhhiephtd@gmail.com>
2017-11-25 18:19:22 +0700

    add reminder

---
+++ src/app/pages/reminder/reminder.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+  
+    angular
+      .module('angular')
+      .controller('ReminderController', ReminderController);
+  
+    /** @ngInject */
+    function ReminderController() {
+        var vm = this;
+    }
+  })();
